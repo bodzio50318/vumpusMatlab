1e0c24462a89d3624858b03f30a4d9079491ec7a
Szymon Grocholski <sajmonidesos@gmail.com>
2016-01-30 16:54:35 +0100

    printWorld and printType colored world drawing

---
+++ printType.m
@@ -0,0 +1,18 @@
+function [] = printType(type,r,c)
+    if type == 0
+        return;
+    end
+
+    dim =4;
+    l=100;
+    if type == 1
+        rectangle('Position',[(c-1)*l -(r)*l l l ],'FaceColor','c','LineWidth',0.01);
+    elseif type == 2
+        rectangle('Position',[(c-1)*l -(r)*l l l ],'FaceColor','r','LineWidth',0.01);
+    elseif type == 3
+        rectangle('Position',[(c-1)*l -(r)*l l l ],'FaceColor','y','LineWidth',0.01);
+    elseif type == 4
+        rectangle('Position',[(c-1)*l+10 -(r)*l+10 20 20 ],'FaceColor','black','LineWidth',0.01);
+    end
+    
+end
---
+++ printWorld.m
@@ -0,0 +1,27 @@
+world = worldGenerator();
+dim = 4;
+ratio=1/dim;
+l = dim*100;
+figure;
+rectangle('Position',[0 -l l l]);
+hold on;
+for i = 1:dim
+    
+    line([0,l],[-i*ratio*l,-i*ratio*l]);
+    line([i*ratio*l,i*ratio*l],[0,-l]);
+end
+
+% for r = 1:dim
+%     for c = 1:dim
+
+for r = 1:dim
+    for c= 1:dim
+        printType(world(r,c).type,r,c)
+        
+        if world(r,c).player == 1
+            printType(4,r,c)
+        end
+        
+    end
+end
+
