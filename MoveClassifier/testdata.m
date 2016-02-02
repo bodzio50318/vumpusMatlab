695f65311753470763c94209c04a9a2d01ebb9ff
Bodzio <matbodzi@gmail.com>
2016-02-02 20:22:17 +0100

    full test data

--- MoveClassifier/testdata.m
+++ MoveClassifier/testdata.m
@@ -1,5 +1,5 @@
 netInputs = [  3 1; 3 2; 3 3; 3 4; 4 1; 4 2; 4 3; 4 4;]'
-neyOutputs= [   1 1 1 0;
+netOutputs= [   1 1 1 0;
                 1 1 1 1;
                 1 1 1 1;
                 1 0 1 1;
@@ -7,3 +7,17 @@ neyOutputs= [   1 1 1 0;
                 1 1 0 1;
                 1 1 0 1;
                 1 0 0 1;]'
+            
+trainData =[1 1 1 1 2 2 2 2;
+            1 2 3 4 1 2 3 4]
+
+
+                
+
+T=[0 0 0 0 1 1 1 1;
+1 1 1 0 1 1 1 0;
+1 1 1 1 1 1 1 1;
+0 1 1 1 0 1 1 1]
+
+netInputs=[trainData,netInputs];
+netOutputs=[T,netOutputs];
