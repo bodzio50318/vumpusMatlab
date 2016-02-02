5e781489d6ffb73a654be8df6a4b785fc17e809c
Bodzio <matbodzi@gmail.com>
2016-02-02 22:48:58 +0100

    DoubleNetworkGame
    Working copy
    Some issues with printWorld

---
+++ Functions/doubleNetGame.m
@@ -0,0 +1,67 @@
+function [ points ] = doubleNetGame(world,netMoveClassify,netMoveDecision )
+
+alive=1;
+points=20;
+
+xi=1;
+yi=1;
+
+d1=1;
+g1=0;
+move1=2;
+
+d=mod((world(1,1).senses(1)+world(1,1).senses(2)),2);
+g=world(1,1).senses(3);
+
+printWorld(world);
+pause;
+
+while alive
+    possibleMoves=netMoveClassify([xi,yi]')
+    moveDecision=netMoveDecision([d1,g1,move1,d,g]')
+    
+    finalProb=possibleMoves.*moveDecision;
+    finalProb=finalProb/norm(finalProb,1);
+    finalProb=cumsum(finalProb);
+    
+    choice=rand();
+    if choice< finalProb(1)
+        move=1;
+    elseif choice< finalProb(2)
+        move=2;
+    elseif choice< finalProb(3)
+        move=3;
+    else
+        move=4;
+    end
+    
+    move
+    
+    [newWorld,alive,points]=nextWorld(world,move,points);
+    
+    printWorld(world);
+    pause;
+    
+    if points<0
+        break;
+    end
+    
+    
+    world=newWorld;
+    
+    [xi,yi]=findPlayer(world);
+    
+    d1=d;
+    g1=g;
+    move1=move;
+    
+    d=mod((world(xi,yi).senses(1)+world(xi,yi).senses(2)),2);
+    g=world(xi,yi).senses(3);
+    
+    
+    
+end
+
+
+end
+
--- performanceDoubleNetPlayer.m
+++ performanceDoubleNetPlayer.m
@@ -7,16 +7,17 @@ numOfGames=1000;
 
 activeDir=pwd;
 
-netMoveClassify = load(strcat(activeDir,'/MoveClassifier/','net'));
-netMoveDecision = load(strcat(activeDir,'/MoveDecision/','net'));
+load(strcat(activeDir,'/MoveClassifier/','netMoveClassify.mat'),'net');
+netMoveClassify = net;
+
+clear net;
+
+load(strcat(activeDir,'/MoveDecision/','netMoveDecision.mat'),'net');
+netMoveDecision =net;
+
 
 addpath(strcat(activeDir,'/Functions'));
-networkName1='manualInput100.mat';
 
-
-load(strcat(activeDir,'/Networks/',networkName1));
-
-%networkName2='manualInput1000.mat';
 
 sumPointsN1=0;
 sumPointsN2=0;
@@ -24,34 +25,18 @@ sumPointsN2=0;
 sumOfWinsN1=0;
 sumOfWinsN2=0;
 
-% figure;
-net1 = loadalkfsldjflsjf
-net2 = load
 
-if(vsRandom==1)
     for i=1:numOfGames
         i
-        [ pointsNet,pointsRandom,netWins,randomWins ]=playVsRandom(net);
-        sumPointsN1=sumPointsN1+pointsNet;
-        sumPointsN2=sumPointsN2+pointsRandom;
-        
-        sumOfWinsN1=sumOfWinsN1+netWins;
-        sumOfWinsN2=sumOfWinsN2+randomWins;
+        world=worldGenerator();
+        points=doubleNetGame(world,netMoveClassify,netMoveDecision);
+        %[netWins,randomWins]=doubleVsRandom(netMoveClassify,netMoveDecision)
+      
+%         sumOfWinsN=sumOfWinsN+netWins;
+%         sumOfWinsR=sumOfWinsR+randomWins;
     end
-else
-     for i=1:numOfGames
-        i
-        [pointsN1, pointsN2] = netVsNet(networkName1,networkName2);
-        sumPointsN1=sumPointsN1+pointsN1;
-        sumPointsN2=sumPointsN2+pointsN2;
 
-        sumOfWinsN1=sumOfWinsN1+net1Wins;
-        sumOfWinsN2=sumOfWinsN2+net2Wins;
-     end
-end
 
-avg1=sumPointsN1/numOfGames
 winRatio1=sumOfWinsN1/numOfGames
 
-avg2=sumPointsN2/numOfGames
 winRatio2=sumOfWinsN2/numOfGames
